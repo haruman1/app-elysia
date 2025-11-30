66703a65230f620358d5bb88c6d4c17b35e5754e
Haruman Wiguna <harumanwiguna1@gmail.com>
2025-12-01 00:19:16 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,47 @@
 import { Elysia, t } from 'elysia';
-
 import { jwt } from '@elysiajs/jwt';
-import Mysql from '../mysql.config';
-import { authRoutes } from './routes/auth';
-import { userRoutes } from './routes/users';
+import { authRoutes } from '../src/routes/auth';
+import { userRoutes } from '../src/routes/users';
 import openapi from '@elysiajs/openapi';
-import { stockRoutes } from './routes/stock';
+import { stockRoutes } from '../src/routes/stock';
 import { cors } from '@elysiajs/cors';
 
-async function bootstrap() {
-  const app = new Elysia()
-    .use(
-      jwt({
-        name: 'jwt',
-        secret: process.env.JWT_SECRET!,
-        exp: '2h',
-      })
-    )
-    .guard({
-      headers: t.Object({
-        authorization: t.Optional(t.String()),
-      }),
+const app = new Elysia()
+  .use(
+    jwt({
+      name: 'jwt',
+      secret: process.env.JWT_SECRET!,
+      exp: '2h',
     })
-    .onError(({ code, error }) => {
-      if (code === 401) {
-        return { success: false, message: 'Unauthorized Access' };
-      }
-      if (code === 'NOT_FOUND') {
-        return {
-          success: false,
-          message: 'Unauthorized Access',
-        };
-      }
+  )
+  .guard({
+    headers: t.Object({
+      authorization: t.Optional(t.String()),
+    }),
+  })
+  .onError(({ code, error }) => {
+    if (code === 401) {
+      return { success: false, message: 'Unauthorized Access' };
+    }
+    if (code === 'NOT_FOUND') {
+      return { success: false, message: 'Unauthorized Access' };
+    }
+  })
+  .get('/', () => 'Hello Elysia')
+  .use(
+    cors({
+      origin: ['*'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
     })
-    .get('/', () => 'Hello Elysia')
-    .use(
-      cors({
-        origin: ['*'], // FE Vite
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
-      })
-    )
-    .use(openapi())
-    .use(authRoutes)
-    .use(userRoutes)
-    .use(stockRoutes)
-    .listen(parseInt(process.env.PORT!));
+  )
+  .use(openapi())
+  .use(authRoutes)
+  .use(userRoutes)
+  .use(stockRoutes);
 
-  console.log(`🚀 Running at http://localhost:${process.env.PORT}`);
-}
+// ❗ Vercel tidak boleh pakai .listen(), jadi hilangkan
+// .listen(parseInt(process.env.PORT!));
 
-bootstrap();
+// Vercel akan menjalankan ini sebagai handler
+export default app;

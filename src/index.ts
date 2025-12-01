c10940e07cb36c9520f2343185a7815c07f8566d
Haruman Wiguna <harumanwiguna1@gmail.com>
2025-12-01 08:49:35 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,28 @@
-// api/index.ts
-import { Elysia, t } from 'elysia';
+import { Elysia } from 'elysia';
+
 import { jwt } from '@elysiajs/jwt';
-import { authRoutes } from '../src/routes/auth';
-import { userRoutes } from '../src/routes/users';
+import { authRoutes } from './routes/auth';
+import { userRoutes } from './routes/users';
 import openapi from '@elysiajs/openapi';
-import { stockRoutes } from '../src/routes/stock';
-import { cors } from '@elysiajs/cors';
+import { stockRoutes } from './routes/stock';
 
-const app = new Elysia()
-  .use(
-    jwt({
-      name: 'jwt',
-      secret: process.env.JWT_SECRET!,
-      exp: '2h',
-    })
-  )
-  .guard({
-    headers: t.Object({
-      authorization: t.Optional(t.String()),
-    }),
-  })
-  .onError(({ code, error }) => {
-    if (code === 401) {
-      return { success: false, message: 'Unauthorized Access' };
-    }
-    if (code === 'NOT_FOUND') {
-      return { success: false, message: 'Unauthorized Access' };
-    }
-  })
-  .get('/', () => 'Hello Elysia')
-  .use(
-    cors({
-      origin: ['*'],
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-    })
-  )
-  .use(openapi())
-  .use(authRoutes)
-  .use(userRoutes)
-  .use(stockRoutes);
+async function bootstrap() {
+  const app = new Elysia()
+    .use(
+      jwt({
+        name: 'jwt',
+        secret: process.env.JWT_SECRET!,
+        exp: '2h',
+      })
+    )
+    .get('/', () => 'Hello Elysia')
+    .use(openapi())
+    .use(authRoutes)
+    .use(userRoutes)
+    .use(stockRoutes)
+    .listen(parseInt(process.env.PORT!));
 
-// ❗ Vercel tidak boleh pakai .listen(), jadi hilangkan
-// .listen(parseInt(process.env.PORT!));
+  console.log(`🚀 Running at http://localhost:${process.env.PORT}`);
+}
 
-// Vercel akan menjalankan ini sebagai handler
-export default app;
+bootstrap();
